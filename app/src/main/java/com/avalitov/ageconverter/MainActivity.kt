43a3ca756eb469a148cd162d52cc9bd003bc4394
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-04-27 16:44:18 +0500

    Converting from date to minutes realized

--- app/src/main/java/com/avalitov/ageconverter/MainActivity.kt
+++ app/src/main/java/com/avalitov/ageconverter/MainActivity.kt
@@ -6,20 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-
-import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var datePickerButton: Button
+    lateinit var tvSelectedDate: TextView
+    lateinit var tvSelectedDateInMinutes: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         datePickerButton = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvSelectedDateInMinutes = findViewById(R.id.tvSelectedDateInMinutes)
 
         datePickerButton.setOnClickListener() { view ->
             clickDatePicker(view)
@@ -30,22 +35,43 @@ class MainActivity : AppCompatActivity() {
 
     fun clickDatePicker(view: View) {
 
+        //getting current date to offer it as a default
         val calendar = Calendar.getInstance()
-        //calendar.firstDayOfWeek = Calendar.MONDAY
-
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
         val datePickerDlg = DatePickerDialog(this,
-                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                    Toast.makeText(this, "The chosen year is $selectedYear, the month is $selectedMonth and the day is $selectedDayOfMonth",
+                            Toast.LENGTH_LONG).show()
+
+                    val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                    tvSelectedDate.text = selectedDate
+
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                    //creating a Date object
+                    val theDate = sdf.parse(selectedDate)
+
+                    val selectedDateInMinutes = theDate!!.time / 1000 / 60
+                    //tvSelectedDate.text = selectedDateInMinutes.toString()
+
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    val currentDateInMinutes = currentDate!!.time / 1000 / 60
+
+                    val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                    tvSelectedDateInMinutes.text = differenceInMinutes.toString()
 
                 }
                 ,year
                 ,month
                 ,day);
-        datePickerDlg.datePicker.firstDayOfWeek = Calendar.MONDAY;
-        datePickerDlg.show();
+
+        //datePickerDlg.datePicker.firstDayOfWeek = Calendar.MONDAY
+        datePickerDlg.datePicker.maxDate = Date().time - (24 * 60 * 60 * 1000) // setting maximum date to yesterday
+        datePickerDlg.show()
 
 //        DatePickerDialog(this,
 //             DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
